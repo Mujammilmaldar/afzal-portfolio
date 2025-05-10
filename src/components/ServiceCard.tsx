a98f35455ca8691bc7142209e60b11610a1e2d7d
Mujammilmaldar <145499416+Mujammilmaldar@users.noreply.github.com>
2025-05-10 18:28:53 +0530

    updated hover effect half

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -10,7 +10,9 @@ export default function ServiceCard({
   description: string;
 }) {
   return (
-    <div className="p-10 shadow-sm hover:shadow-md transition flex gap-4">
+    <div className="cards-style">
+
+    <div className="main-card-service p-10 relative shadow-sm hover:shadow-md transition flex gap-4 overflow-hidden">
       <div className="text-primary text-4xl">{icon}</div>
       <div className="content px-4">
         <h3 className="text-lg font-semibold text-dark cursor-pointer text-left mb-3 hover:text-primary">
@@ -18,6 +20,16 @@ export default function ServiceCard({
         </h3>
         <p className="text-gray-500 text-sm text-left mb-4">{description}</p>
       </div>
+    </div>
+    <div className="upside-conetnt absolute sec-card-blur -z-1"></div>
+    <div className="down-side-content sec-card-content absolute -z-1">
+      <div className="card-heading">
+        <h2>Card Benefits for Afzal</h2>
+      </div>
+      <div className="content">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere praesentium vel laborum!</p>
+      </div>
+    </div>
     </div>
   );
 }
