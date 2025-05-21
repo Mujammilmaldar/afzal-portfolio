b306d5c9318c6a6bc8a5338a414dc9bb31d08fc8
Mujammil Maldar <mujammilmaldar65@gmail.com>
2025-05-21 19:56:11 +0530

    updated the remaining content

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -19,7 +19,7 @@ export default function ServicesSection() {
           <ServiceCard
             icon={<SlRocket />}
             title="Healthcare SEO"
-            description="Optimizing your medical website to rank higher in Google and AI-generated search results for healthcare-related queries."
+            description="Optimizing your medical website to rank higher in Google and AI-generated search results for healthcare related queries."
             benefits="Benefits"
             benefitsdesc = "Drives organic traffic, attracts targeted patients, builds credibility & trust, and delivers long-term, cost-effective growth."
           />
